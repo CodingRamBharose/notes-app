3b71f8a5f4a76336b407739a43bb5e4d8bd0d645
Ram Avtar <ramavtarsharma85507@gmail.com>
2024-06-12 07:21:51 +0530

    initial stage

--- notesapp/src/App.jsx
+++ notesapp/src/App.jsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import Signup from './pages/Signup/Signup'
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-const routes = (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/dashboard' exact element={<Home/>}/>
-      <Route path='/login' exact element={<Login/>}/>
-      <Route path='/signup' exact element={<Signup/>}/>
-    </Routes>
-  </BrowserRouter>
-);
-
 
 const App = () => {
   return (
     <div>
       <ToastContainer
-      position="top-right"
-      autoClose={2000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      draggable
-      toastClassName="rounded-md p-3 mt-16 "
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        toastClassName="rounded-md p-3 mt-16"
       />
-      {routes}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
